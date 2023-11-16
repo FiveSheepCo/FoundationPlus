b008ef747b20f364981b0cd926bc14c5257a134f
SplittyDev <splittydev@protonmail.com>
2023-11-16 23:49:54 +0100

    Make the package modular

--- QuintKit/Package.swift
+++ QuintKit/Package.swift
@@ -10,18 +10,55 @@ let package = Package(
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // The full QuintKit package with all modules included.
         .library(
             name: "QuintKit",
-            targets: ["QuintKit"]),
+            targets: ["QuintKitCore", "QuintKitAlerting", "QuintKitNetworking", "QuintKitUI"]
+        ),
+        
+        // Core module.
+        // Provides foundational extensions, general helpers and shared core functionality.
+        .library(
+            name: "QuintKit Core",
+            targets: ["QuintKitCore"]
+        ),
+        
+        // Alerting module.
+        // Provides an easier way to display cross-platform alerting prompts.
+        .library(
+            name: "QuintKit Alerting",
+            targets: ["QuintKitAlerting"]
+        ),
+        
+        // Networking module.
+        // Provides an easier way to accomplish complex asynchronous networking tasks.
+        .library(
+            name: "QuintKit Networking",
+            targets: ["QuintKitNetworking"]
+        ),
+        
+        // UI module.
+        // Provides cross-platform UI components.
+        .library(
+            name: "QuintKit UI",
+            targets: ["QuintKitUI"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "QuintKit"),
+        .target(name: "QuintKitCore"),
+        .target(name: "QuintKitAlerting", dependencies: ["QuintKitCore"]),
+        .target(name: "QuintKitNetworking", dependencies: ["QuintKitCore"]),
+        .target(name: "QuintKitUI", dependencies: ["QuintKitCore"]),
         .testTarget(
             name: "QuintKitTests",
-            dependencies: ["QuintKit"]),
+            dependencies: [
+                "QuintKitCore",
+                "QuintKitAlerting",
+                "QuintKitNetworking",
+                "QuintKitUI"
+            ]
+        ),
     ]
 )
--- QuintKit/Sources/QuintKit/QuintKit.swift
+++ QuintKit/Sources/QuintKit/QuintKit.swift
@@ -1,2 +1,9 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+// This is a placeholder for the full QuintKit target.
+//
+// All code is located in the QuintKit module folders:
+// - QuintKitCore
+// - QuintKitAlerting
+// - QuintKitNetworking
+// - QuintKitUI
+//
