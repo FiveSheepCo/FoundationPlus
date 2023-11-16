aded340012349865eb14ae519dd881096ca1cc2e
SplittyDev <splittydev@protonmail.com>
2023-11-17 00:08:35 +0100

    Docs: Improve TimeInterval extension docs

--- QuintKit/Sources/QuintKitCore/Extensions/TimeInterval/TimeInterval+SubUnitComponents.swift
+++ QuintKit/Sources/QuintKitCore/Extensions/TimeInterval/TimeInterval+SubUnitComponents.swift
@@ -2,30 +2,87 @@ import Foundation
 
 extension TimeInterval {
     
-    /// Get the day component of the time interval.
+    /// Retrieves the day component of the time interval,
+    /// representing the number of days within the current year.
+    ///
+    /// This property calculates the day component by dividing the time interval by the number of seconds in a day,
+    /// then taking the remainder of division by 365. This represents the count of days not accounting for leap years.
+    ///
+    /// Example usage:
+    /// ```swift
+    /// let interval = TimeInterval(400000) // Represents a time interval
+    /// let days = interval.dayComponent // Number of days within the year
+    /// ```
+    ///
+    /// - Returns: The day component as a Double, disregarding leap years.
     public var dayComponent: Double {
         days.truncatingRemainder(dividingBy: 365)
     }
     
-    /// Get the hour component of the time interval.
+    /// Retrieves the hour component of the time interval,
+    /// representing the number of complete hours not forming a complete day.
+    ///
+    /// This property isolates the hour component by finding the remainder of the time interval's hour count when divided by 24,
+    /// effectively extracting the hour part that doesn't compose a full day.
+    ///
+    /// Example usage:
+    /// ```swift
+    /// let interval = TimeInterval(10000) // Represents a time interval
+    /// let hours = interval.hourComponent // Hours not composing a full day
+    /// ```
+    ///
+    /// - Returns: The hour component as a Double.
     public var hourComponent: Double {
         hours.truncatingRemainder(dividingBy: 24)
     }
     
-    /// Get the minute component of the time interval.
+    /// Retrieves the minute component of the time interval,
+    /// representing the number of complete minutes not forming a complete hour.
+    ///
+    /// This property calculates the minute component by finding the remainder of the time interval's minute count when divided by 60,
+    /// effectively extracting the minute part that doesn't compose a full hour.
+    ///
+    /// Example usage:
+    /// ```swift
+    /// let interval = TimeInterval(3600) // Represents 1 hour
+    /// let minutes = interval.minuteComponent // Should be 0, as there are no extra minutes
+    /// ```
+    ///
+    /// - Returns: The minute component as a Double.
     public var minuteComponent: Double {
         minutes.truncatingRemainder(dividingBy: 60)
     }
     
-    /// Get the second component of the time interval.
+    /// Retrieves the second component of the time interval,
+    /// representing the number of seconds not forming a complete minute.
+    ///
+    /// This property isolates the second component by finding the remainder of the time interval's second count when divided by 60,
+    /// effectively extracting the second part that doesn't compose a full minute.
+    ///
+    /// Example usage:
+    /// ```swift
+    /// let interval = TimeInterval(65) // Represents 65 seconds
+    /// let seconds = interval.secondComponent // Should be 5, as there's 1 minute and 5 seconds
+    /// ```
+    ///
+    /// - Returns: The second component as a Double.
     public var secondComponent: Double {
         self.truncatingRemainder(dividingBy: 60)
     }
     
-    /// Get the `DateComponents` of this time interval.
+    /// Converts the time interval into `DateComponents`, encapsulating the day, hour, minute, and second components.
     ///
-    /// The returned value is in an unspecified calendar and timezone.
-    /// Only the `day`, `hour`, `minute` and `second` components are set.
+    /// This method constructs a `DateComponents` object from the time interval, setting only the `day`, `hour`, `minute`, and `second` fields.
+    /// It's important to note that the returned `DateComponents` are in an unspecified calendar and timezone,
+    /// and they represent only the respective components without considering a full calendar context.
+    ///
+    /// Example usage:
+    /// ```swift
+    /// let interval = TimeInterval(100000) // Represents a time interval
+    /// let components = interval.dateComponents // `DateComponents` representing the interval
+    /// ```
+    ///
+    /// - Returns: A `DateComponents` object containing day, hour, minute, and second components.
     public var dateComponents: DateComponents {
         DateComponents(
             day: Int(dayComponent),
--- QuintKit/Sources/QuintKitCore/Extensions/TimeInterval/TimeInterval+SubUnits.swift
+++ QuintKit/Sources/QuintKitCore/Extensions/TimeInterval/TimeInterval+SubUnits.swift
@@ -2,22 +2,62 @@ import Foundation
 
 extension TimeInterval {
     
-    /// Get the amount of days in the time interval.
+    /// Retrieves the total number of days in the time interval.
+    /// This property divides the interval's total seconds by 86,400 (seconds in a day) to calculate
+    /// the total days, including fractional parts.
+    ///
+    /// Example:
+    /// ```swift
+    /// let interval = TimeInterval(172800) // Represents 2 days
+    /// let totalDays = interval.days // 2.0
+    /// ```
+    ///
+    /// - Returns: Total number of days as a Double.
     public var days: Double {
         self / 86400
     }
     
-    /// Get the amount of hours in the time interval.
+    /// Retrieves the total number of hours in the time interval.
+    /// This property divides the interval's total seconds by 3,600 (seconds in an hour) to calculate
+    /// the total hours, including fractional parts.
+    ///
+    /// Example:
+    /// ```swift
+    /// let interval = TimeInterval(10800) // Represents 3 hours
+    /// let totalHours = interval.hours // 3.0
+    /// ```
+    ///
+    /// - Returns: Total number of hours as a Double.
     public var hours: Double {
         self / 3600
     }
     
-    /// Get the amount of minutes in the time interval.
+    /// Retrieves the total number of minutes in the time interval.
+    /// This property divides the interval's total seconds by 60 to calculate the total minutes,
+    /// including fractional parts.
+    ///
+    /// Example:
+    /// ```swift
+    /// let interval = TimeInterval(600) // Represents 10 minutes
+    /// let totalMinutes = interval.minutes // 10.0
+    /// ```
+    ///
+    /// - Returns: Total number of minutes as a Double.
     public var minutes: Double {
         self / 60
     }
     
-    /// Get the amount of seconds in the time interval.
+    /// Retrieves the total number of seconds in the time interval.
+    /// This property directly represents the interval's value in seconds, useful for clarity when
+    /// manipulating or displaying time intervals.
+    ///
+    /// Example:
+    /// ```swift
+    /// let interval = TimeInterval(120) // Represents 120 seconds
+    /// let totalSeconds = interval.seconds // 120.0
+    /// ```
+    ///
+    /// - Returns: Total number of seconds as a Double.
     public var seconds: Double {
         self
     }
