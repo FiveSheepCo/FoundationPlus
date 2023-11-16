9b8f91a75f7a8dcfbf2547b88d183494b5b50066
SplittyDev <splittydev@protonmail.com>
2023-11-16 23:51:03 +0100

    Core: Add `String+ObjectPlacementMarkers`
    Removes object placement markers from a string.

---
+++ QuintKit/Sources/QuintKitCore/Extensions/String/String+ObjectReplacementMarkers.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension String {
+    /// Returns a new `String` without Unicode Object Replacement Markers (`0xfffc`).
+    ///
+    /// **Context**
+    ///
+    /// For some reason, the dictation feature inserts this marker at the end of the string.
+    /// For RTL languages like arabic, it's at the beginning of the string.
+    public func removingObjectReplacementMarkers() -> String {
+        self.replacingOccurrences(of: "\u{fffc}", with: "", options: String.CompareOptions.literal, range: nil)
+    }
+}
---
+++ QuintKit/Tests/QuintKitTests/QuintKitCore/Extensions/StringTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import QuintKitCore
+
+final class Core__StringTests: XCTestCase {
+    func testRemovingObjectReplacementMarkers() throws {
+        let subject = "Hello world!\u{fffc}"
+        let expected = "Hello world!"
+        XCTAssertEqual(subject.removingObjectReplacementMarkers(), expected)
+    }
+}
