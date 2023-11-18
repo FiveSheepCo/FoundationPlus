91f8bd919673648e3685c15ddda87b8e3ede08f2
SplittyDev <splittydev@protonmail.com>
2023-11-18 01:19:53 +0100

    UI: Add `Hashable+Share`

---
+++ QuintKit/Sources/QuintKitUI/UIKit/Extensions/Hashable/Hashable+Share.swift
@@ -0,0 +1,16 @@
+#if canImport(UIKit)
+
+import Foundation
+import UIKit
+
+public extension Hashable {
+    
+    /// Shares the responder using a `UIActivityViewController`.
+    @available(iOSApplicationExtension, unavailable)
+    func share(applicationActivities: [UIActivity]? = nil) {
+        let controller = UIActivityViewController(activityItems: [self], applicationActivities: applicationActivities)
+        controller.show(type: .present)
+    }
+}
+
+#endif
