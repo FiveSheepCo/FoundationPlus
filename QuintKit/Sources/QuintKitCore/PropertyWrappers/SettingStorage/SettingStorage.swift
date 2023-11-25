bc6ce5caf244aff91a1c163d41706685608ff5c1
SplittyDev <splittydev@protonmail.com>
2023-11-25 23:04:53 +0100

    Core: Move `SettingStorage` to subfolder

--- QuintKit/Sources/QuintKitCore/PropertyWrappers/SettingStorage/SettingStorage.swift
+++ QuintKit/Sources/QuintKitCore/PropertyWrappers/SettingStorage/SettingStorage.swift
@@ -3,9 +3,23 @@
 import Foundation
 import Combine
 
+/// A property wrapper that provides persistent storage for a value in `UserDefaults` and publishes changes.
+///
+/// `SettingStorage` is designed to store and retrieve values from `UserDefaults` while also providing
+/// a Combine publisher to observe changes to these values. It's useful for settings or preferences in
+/// an app where changes need to be observed and persisted across app launches.
+///
+/// Usage:
+/// ```
+/// class SettingsViewModel: ObservableObject {
+///     @SettingStorage(key: "mySettingKey") var mySetting: String
+/// }
+/// ```
+///
+/// In this example, `mySetting` will be stored in and retrieved from `UserDefaults`, and any changes
+/// to it will be published to subscribers.
 @propertyWrapper
 public struct SettingStorage<Value>: Publishable {
-    
     private let key: String
     private var value: Value
     private let userDefaults: UserDefaults
@@ -29,7 +43,6 @@ public struct SettingStorage<Value>: Publishable {
     // - MARK: Publishable
     
     public var publisher: PublishablePublisher<Value>?
-    
     public var objectWillChange: ObservableObjectPublisher?
 }
 
