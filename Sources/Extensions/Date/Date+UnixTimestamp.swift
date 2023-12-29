aec9e704c9f7d0151580c157223edede1b1e0ecc
SplittyDev <splittydev@protonmail.com>
2023-12-29 04:24:44 +0100

    Add `Date.unixTimestamp`

---
+++ Sources/Extensions/Date/Date+UnixTimestamp.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public extension Date {
+    
+    /// The Unix epoch timestamp in seconds.
+    ///
+    /// This property’s value is negative if the date object is earlier than 00:00:00 UTC on 1 January 1970.
+    var unixTimestamp: Int {
+        Int(floor(self.timeIntervalSince1970))
+    }
+}
--- Tests/Extensions/DateTests.swift
+++ Tests/Extensions/DateTests.swift
@@ -8,4 +8,11 @@ final class Core__DateTests: XCTestCase {
     // TimeIntervalUntilNow always operates on the `now` date,
     // and it's not straight-forward to test in a reproducible way.
     // TODO: Revisit date tests
+    
+    func testUnixTimestamp() {
+        let referenceDate = Date(timeIntervalSince1970: 1703818401.5)   // Fri Dec 29 2023 03:53:21:500 GMT+0100 (CEST)
+        let expectedTimestamp = 1703818401
+        
+        XCTAssertEqual(referenceDate.unixTimestamp, expectedTimestamp)
+    }
 }
