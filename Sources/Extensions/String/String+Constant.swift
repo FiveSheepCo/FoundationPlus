6f28a6da046988ed017ab4ddf016c0f84274654d
SplittyDev <splittydev@protonmail.com>
2024-01-26 20:18:51 +0100

    Add `String` constants

--- Sources/Extensions/String/String+Constant.swift
+++ Sources/Extensions/String/String+Constant.swift
@@ -4,4 +4,13 @@ public extension String {
 
     /// A static constant representing an empty string.
     static let empty: String = ""
+    
+    /// A static constant representing a space (" ").
+    static let space: String = Character.space.toString
+    
+    /// A static constant representing a newline ("\n").
+    static let newline: String = Character.newline.toString
+    
+    /// A static constant representing a tab stop ("\t").
+    static let tab: String = Character.tab.toString
 }
