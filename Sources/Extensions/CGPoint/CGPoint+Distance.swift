da0452ebf9b7dc5f0e03318fa9198518237e50db
SplittyDev <splittydev@gmail.com>
2024-10-21 16:26:38 +0700

    Add `CGPoint.distance(to: CGRect)`

--- Sources/Extensions/CGPoint/CGPoint+Distance.swift
+++ Sources/Extensions/CGPoint/CGPoint+Distance.swift
@@ -10,12 +10,25 @@ public extension CGPoint {
     /// This method computes the Euclidean distance between the current point and another point.
     /// The distance is calculated using the Pythagorean theorem, ensuring accuracy in a 2D coordinate space.
     ///
-    /// - Parameter to: The CGPoint to which the distance is measured.
-    /// - Returns: The distance as a CGFloat.
+    /// - Parameter to: The `CGPoint` to which the distance is measured.
+    /// - Returns: The distance as a `CGFloat`.
     func distance(to point: CGPoint) -> CGFloat {
-        let xDist = self.x - point.x
-        let yDist = self.y - point.y
-        return CGFloat(sqrt(pow(xDist, 2) + pow(yDist, 2)))
+        let dx = self.x - point.x
+        let dy = self.y - point.y
+        return sqrt(dx * dx + dy * dy)
+    }
+    
+    /// Calculates the distance from this point to the closest edge of the specified `CGRect`.
+    ///
+    /// This method computes the Euclidean distance between the current point and the closest edge of a `CGRect`.
+    /// The distance is calculated using the Pythagorean theorem, ensuring accuracy in a 2D coordinate space.
+    ///
+    /// - Parameter to: The `CGRect` to which the distance is measured.
+    /// - Returns: The distance as a `CGFloat`.
+    func distance(to rect: CGRect) -> CGFloat {
+        let dx = max(rect.minX - self.x, 0, self.x - rect.maxX)
+        let dy = max(rect.minY - self.y, 0, self.y - rect.maxY)
+        return sqrt(dx * dx + dy * dy)
     }
 }
 
--- Tests/Extensions/CGPointTests.swift
+++ Tests/Extensions/CGPointTests.swift
@@ -7,7 +7,7 @@ import CoreGraphics
 
 final class CGPointTests: XCTestCase {
 
-    func testDistanceCalculation() {
+    func testPointDistanceCalculation() {
         // Define two points
         let pointA = CGPoint(x: 0, y: 0)
         let pointB = CGPoint(x: 3, y: 4) // 3-4-5 right triangle for easy validation
@@ -20,6 +20,26 @@ final class CGPointTests: XCTestCase {
 
         // Assert that calculated distances are equal to expected distance
         XCTAssertEqual(distanceTo, expectedDistance, accuracy: 0.0001, "Distance to calculation is incorrect.")
+    }
+    
+    func testRectDistanceCalculation() {
+        let sourcePoint = CGPoint(x: 0, y: 0)
+        let targetRect = CGRect(x: 3, y: 4, width: 100, height: 100)
+        
+        // Calculate distance
+        let distanceTo = sourcePoint.distance(to: targetRect)
+        
+        XCTAssertEqual(distanceTo, 5.0, accuracy: 0.0001, "Distance to calculation is incorrect.")
+    }
+    
+    func testRectDistanceCalculation2() {
+        let sourcePoint = CGPoint(x: 0, y: 0)
+        let targetRect = CGRect(x: -5, y: -5, width: 2, height: 1)
+        
+        // Calculate distance
+        let distanceTo = sourcePoint.distance(to: targetRect)
+        
+        XCTAssertEqual(distanceTo, 5.0, accuracy: 0.0001, "Distance to calculation is incorrect.")
     }
 }
 
