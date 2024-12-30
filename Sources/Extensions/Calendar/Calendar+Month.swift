b5be2825c4d4128be2c85036188719952292e951
SplittyDev <splittydev@protonmail.com>
2024-12-30 12:19:57 +0700

    Move `Calendar` day functions to `Calendar+Day`

--- Sources/Extensions/Calendar/Calendar+Month.swift
+++ Sources/Extensions/Calendar/Calendar+Month.swift
@@ -38,16 +38,4 @@ public extension Calendar {
         guard let date = self.date(byAdding: DateComponents(day: -1), to: date) else { return nil }
         return self.startOfDay(for: date)
     }
-    
-    /// Calculates the last moment of a given `Date`.
-    func endOfDay(for date: Date) -> Date? {
-        guard let nextDay = self.date(byAdding: .day, value: 1, to: date) else { return nil }
-        return self.date(byAdding: .second, value: -1, to: self.startOfDay(for: nextDay))
-    }
-    
-    /// Calculates the last moment of a given `Date`.
-    @available(*, deprecated, message: "Use `endOfDay(for:)`, which is consistent with the `startOfDay(for:)`.")
-    func endOfDay(_ date: Date) -> Date? {
-        self.endOfDay(for: date)
-    }
 }
