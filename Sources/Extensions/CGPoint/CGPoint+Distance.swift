e59e6b89130f5ef93ae173bf462ac4bc0dec6150
SplittyDev <splittydev@protonmail.com>
2026-05-25 19:30:26 +0700

    Add `distanceSquared` to CGPoint extensions

--- Sources/Extensions/CGPoint/CGPoint+Distance.swift
+++ Sources/Extensions/CGPoint/CGPoint+Distance.swift
@@ -12,10 +12,9 @@ public extension CGPoint {
     ///
     /// - Parameter point: The `CGPoint` to which the distance is measured.
     /// - Returns: The distance as a `CGFloat`.
+    @inlinable
     func distance(to point: CGPoint) -> CGFloat {
-        let dx = self.x - point.x
-        let dy = self.y - point.y
-        return sqrt(dx * dx + dy * dy)
+        sqrt(distanceSquared(to: point))
     }
     
     /// Calculates the distance from this point to the closest edge of the specified `CGRect`.
@@ -25,10 +24,39 @@ public extension CGPoint {
     ///
     /// - Parameter rect: The `CGRect` to which the distance is measured.
     /// - Returns: The distance as a `CGFloat`.
+    @inlinable
     func distance(to rect: CGRect) -> CGFloat {
-        let dx = max(rect.minX - self.x, 0, self.x - rect.maxX)
-        let dy = max(rect.minY - self.y, 0, self.y - rect.maxY)
-        return sqrt(dx * dx + dy * dy)
+        sqrt(distanceSquared(to: rect))
+    }
+    
+    /// Calculates the squared distance from this point to another specified point.
+    ///
+    /// This can be useful when the actual Euclidean distance isn't necessary, for example
+    /// when comparing distances or sorting by distance.
+    ///
+    /// - Parameter point: The `CGPoint` to which the distance is measured.
+    /// - Returns: The distance as a `CGFloat`.
+    @inlinable
+    func distanceSquared(to point: CGPoint) -> CGFloat {
+        let dx = self.x - point.x
+        let dy = self.y - point.y
+        return dx * dx + dy * dy
+    }
+    
+    /// Calculates the squared distance from this point to the closest edge of the specified `CGRect`.
+    ///
+    /// This can be useful when the actual Euclidean distance isn't necessary, for example
+    /// when comparing distances or sorting by distance.
+    ///
+    /// - Parameter rect: The `CGRect` to which the distance is measured.
+    /// - Returns: The distance as a `CGFloat`.
+    @inlinable
+    func distanceSquared(to rect: CGRect) -> CGFloat {
+        let closestX = min(max(self.x, rect.minX), rect.maxX)
+        let closestY = min(max(self.y, rect.minY), rect.maxY)
+        let dx = self.x - closestX
+        let dy = self.y - closestY
+        return dx * dx + dy * dy
     }
 }
 
